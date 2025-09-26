97161503c0c731edb8e552169a9481614cf7f797
marcelo-ggv <marcelo@grupoggv.com>
2025-09-26 12:47:41 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+// Apenas um exemplo de interatividade inicial
+document.querySelectorAll('.portal-link').forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    alert("Em breve este portal abrirá a página específica!");
+  });
+});
 
